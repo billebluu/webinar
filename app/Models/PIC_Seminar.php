0348040c3b6c507aa2e841e8a746d9c810e93b99
sekar gesti a. u <sekargestiau@gmail.com>
2023-06-05 00:53:45 +0700

    update terbaru model

---
+++ app/Models/PIC_Seminar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PIC_Seminar extends Model
+{
+    use HasFactory;
+    protected $table = "pic_seminar";
+
+}
