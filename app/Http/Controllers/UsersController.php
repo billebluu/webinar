d0225c2ac112506464949293a0dbf880cb11bbcc
sekar gesti a. u <sekargestiau@gmail.com>
2023-07-06 20:40:56 +0700

    update login, register, logout admin & user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,6 +14,11 @@ class UsersController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         // $datas = User::all();
